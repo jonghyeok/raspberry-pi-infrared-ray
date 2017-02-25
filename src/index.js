a0eb9be6cd3d619266aa0348564cfab1fdc277ce
snail.J <jjhsnail@naver.com>
2017-02-25 23:05:48 +0900

    add receiverIR source

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import 'babel-polyfill';
 import Gpio from 'tm-onoff';
 
+const receiverIR = new Gpio(18, 'in', 'both');
 
-console.log('Yay!');
+  console.info("System is started now!");
+
+
+receiverIR.watch(async function(err, value){
+  if (err) {
+      console.error(err);
+  }
+
+  console.info(value);
+
+});

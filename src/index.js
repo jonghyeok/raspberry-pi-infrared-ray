16448aec5e06735d41a70832b3c94102d1c975fe
snail.J <jjhsnail@naver.com>
2017-03-13 18:23:54 +0900

    send source

--- src/index.js
+++ src/index.js
@@ -3,18 +3,28 @@ import 'babel-polyfill';
 
 var sensor = require('node-dht-sensor');
 
-setTimeout(loop, 1000);
+var http = require('http');
+http.post = require('http-post');
+
+setTimeout(loop, 5000);
 
 function loop() {
   sensor.read(22, 18, function(err, temperature, humidity) {
       if (!err) {
+        http.post('http://asiancedev.cafe24.com/temp/do.php', { r: 'add_data', temp: temperature, humi : humidity , device_id : 'studio_room_tmp'  }, function(res){
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+            });
+        });
+
+
           console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
               'humidity: ' + humidity.toFixed(1) + '%'
           );
       }
   });
 
-  setTimeout(loop, 1000);
+  setTimeout(loop, 5000);
 }
 
 
